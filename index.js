290f7a046929c920248289754b5b0623242d4f07
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-09-30 21:36:17 -0300

    last commit

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const url = 'https://www.theguardian.com/international'
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+
+        console.log(articles)
     })
